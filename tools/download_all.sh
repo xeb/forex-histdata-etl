f90507e6cb98dfa05ae07c3410cfa5a32c3e3d2a
Mark Kockerbeck <mark@kockerbeck.com>
2016-02-11 13:14:25 -0800

    Adding gitignore

--- tools/download_all.sh
+++ tools/download_all.sh
@@ -2,12 +2,13 @@
 # Downloads all ticker data from histdata.com
 # Requires pup
 
-export ALL_TICKERS=$(curl -vvv http://www.histdata.com/download-free-forex-data/?/ascii/1-minute-bar-quotes  | pup 'a attr{href}' | grep -i quotes | grep -o '......$')
+export ALL_TICKERS=$(curl http://www.histdata.com/download-free-forex-data/?/ascii/1-minute-bar-quotes  | pup 'a attr{href}' | grep -i quotes | grep -o '......$')
 
 for t in $ALL_TICKERS; do
     for i in `seq 2000 2017`; do
         echo "Downloading $t with $i..."
         ./download.sh --ticker="$t" --year="$i"
         rm -rf *.txt # I don't care about the gaps
+        mv *.csv ../data/
     done
 done
