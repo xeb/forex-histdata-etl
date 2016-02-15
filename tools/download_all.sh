d796c9944b6f2b9df08436d5525292ce2f890a87
Mark Kockerbeck <mark@kockerbeck.com>
2016-02-15 14:17:23 -0800

    Getting current months and cleanup

--- tools/download_all.sh
+++ tools/download_all.sh
@@ -5,10 +5,47 @@
 export ALL_TICKERS=$(curl http://www.histdata.com/download-free-forex-data/?/ascii/1-minute-bar-quotes  | pup 'a attr{href}' | grep -i quotes | grep -o '......$')
 
 for t in $ALL_TICKERS; do
-    for i in `seq 2000 2017`; do
-        echo "Downloading $t with $i..."
-        ./download.sh --ticker="$t" --year="$i"
-        rm -rf *.txt # I don't care about the gaps
-        mv *.csv ../data/
+    for i in `seq 2016 $(date +%Y)`; do
+
+    	FILE="../data/$t$i.csv"
+    	if [[ -s $FILE ]]; then
+    		echo "File $FILE exists, skipping..."
+    		continue
+    	fi
+
+    	if [ $i == $(date +%Y) ]; then
+    		# Download each month
+    		echo "CURRENT YEAR; Downloading up to month $(date +%m)..."
+    		for j in `seq 1 $(date +%m)`; do
+    			echo "Downloading $t for $i (month $j)..."
+    			FILE="../data/$t$i`printf "%02d\n" ${j}`.csv"
+    			if [[ -s $FILE ]]; then
+		    		echo "File $FILE exists, skipping..."
+		    		continue
+		    	else
+        			./get_zip.sh --ticker="$t" --year="$i" --month="$j"
+    			fi
+    		done
+        else
+        	# Download a single year
+	        echo "Downloading $t with $i..."
+	        ./get_zip.sh --ticker="$t" --year="$i"
+    	fi
     done
 done
+
+# Unzip everything
+unzip \*.zip
+
+# Delete all zips files
+rm -rf *.zip 2> /dev/null
+
+# Delete all gap files
+rm -rf *.txt  2> /dev/null
+
+# Move all CSVs to the data directory
+mv *.csv ../data/ 2> /dev/null
+
+# Change the names
+rename -d DAT_ASCII_ ../data/* 2> /dev/null
+rename -d _M1_ ../data/* 2> /dev/null
--- tools/get_zip.sh
+++ tools/get_zip.sh
@@ -12,7 +12,8 @@ while test $# -gt 0; do
                         echo "arguments:"
                         echo "-h, --help                show brief help (this)"
                         echo "-t, --ticker=TICKER       specify an action to use"
-                        echo "-y, --year=YEAR           specify a directory to store output in"
+                        echo "-y, --year=YEAR           the year to download"
+                        echo "-m, --month=MONTH         the month to download"
                         exit 0
                         ;;
                 -t)
@@ -43,6 +44,20 @@ while test $# -gt 0; do
                         export YEAR=`echo $1 | sed -e 's/^[^=]*=//g'`
                         shift
                         ;;
+                -m)
+                        shift
+                        if test $# -gt 0; then
+                                export MONTH=$1
+                        else
+                                echo "no month specified"
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --month*)
+                        export MONTH=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;
                 *)
                         break
                         ;;
@@ -56,20 +71,31 @@ if [[ -z "$YEAR" ]]; then
     echo "Year not set"
     exit 2
 fi
+# Get the special "TK" token value...
+export TK_URL="http://www.histdata.com/download-free-forex-historical-data/?/ascii/1-minute-bar-quotes/${TICKER}/${YEAR}"
 
-export MONTH=$YEAR
+if [[ -z "$MONTH" ]]; then
+    echo "Month not set, using year..."
+    export MONTH=$YEAR # we are assuming that with no month, we use the year-based scheme for downloading
+else
+    # Month was set, so use it in the URL
+    export MONTH=`printf "%02d\n" ${MONTH}`
+    echo "Month is $MONTH now; resetting TK URL"
+    export TK_URL="http://www.histdata.com/download-free-forex-historical-data/?/ascii/1-minute-bar-quotes/${TICKER}/${YEAR}/${MONTH}"
+fi
+
 echo "Using ticker '$TICKER' and year of '$YEAR' and month of '$MONTH'"
+export TK=$(curl -s ${TK_URL} | grep "name=\"tk\"" | head -n 1 | sed -e "s/.* value=\"\(.*\)\".*/\1/")
 
-export TK=$(curl -s http://www.histdata.com/download-free-forex-historical-data/?/ascii/1-minute-bar-quotes/${TICKER}/${YEAR} | grep "name=\"tk\"" | head -n 1 | sed -e "s/.* value=\"\(.*\)\".*/\1/")
 
 if [[ -z "$TK" ]]; then
     echo "Could not get TK value"
     exit 3
 fi
 
-echo "TK value is $TK"
+# echo "TK value is $TK"
 
-export FILE_PATH="output-$TICKER-$YEAR.zip"
+export FILE_PATH="output-$TICKER-$YEAR-$MONTH.zip"
 
 echo "Downloading $FILE_PATH..."
 
@@ -87,14 +113,12 @@ Chrome/48.0.2564.97 Safari/537.36' \
     -H 'Referer: http://www.histdata.com/download-free-forex-historical-data/?/ascii/1-minute-bar-quotes/eurusd/2000' \
     -H 'Connection: keep-alive' \
     -H 'DNT: 1' \
-    --data "tk=$TK&date=$YEAR&datemonth=$MONTH&platform=ASCII&timeframe=M1&fxpair=$TICKER" \
+    --data "tk=$TK&date=$YEAR&datemonth=$YEAR$MONTH&platform=ASCII&timeframe=M1&fxpair=$TICKER" \
     --compressed > $FILE_PATH
 if [[ -s $FILE_PATH ]]; then
-    unzip -u $FILE_PATH
     echo "Done!"
+    exit 0
 else
     echo "Failed to download, check year"
     exit 3
 fi
-rm -f $FILE_PATH
-exit 0
